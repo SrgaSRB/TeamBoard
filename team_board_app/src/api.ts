d8f3a3cd799260b3c7b8fb843bfed063e52db120
SrgaSRB <srdjandelic02@gmail.com>
2025-04-18 00:10:19 +0200

    change service ip from localhost to render ip

--- team_board_app/src/api.ts
+++ team_board_app/src/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5219/api",
+    baseURL: "https://teamboard.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
       }
