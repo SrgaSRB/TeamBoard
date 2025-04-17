5aeb146a761b149d5c10d3dc246ff8292e953ab1
SrgaSRB <srdjandelic02@gmail.com>
2025-04-18 01:10:45 +0200

    fix chart on dashboard for mobile devices

--- team_board_app/src/components/Dashboard.tsx
+++ team_board_app/src/components/Dashboard.tsx
@@ -1,26 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import api from "../api";
 import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
-interface Team{
+interface Team {
     teamName: string;
     teamDescription: string;
     projects: string[];
 }
 
-interface MyProject{
+interface MyProject {
     projectName: string;
     projectDescription: string;
     tasks: string[];
 }
 
-interface HighPriorityTask{
+interface HighPriorityTask {
     taskName: string;
     taskDescription: string;
     status: string;
 }
 
-interface TaskStatus{
+interface TaskStatus {
     status: string;
     count: number;
 }
@@ -33,125 +33,145 @@ const Dashboard: React.FC = () => {
     const [highPriorityTasks, setHighPriorityTasks] = React.useState<HighPriorityTask[]>([]);
     const [taskStatus, setTaskStatus] = React.useState<TaskStatus[]>([]);
 
+    const [radius, setRadius] = useState(80);
+
+    useEffect(() => {
+        const updateRadius = () => {
+            if (window.innerWidth < 480) {
+                setRadius(60); 
+            } else if (window.innerWidth < 768) {
+                setRadius(70);
+            } else {
+                setRadius(80); 
+            }
+        };
+
+        updateRadius(); 
+        window.addEventListener("resize", updateRadius); 
+        return () => window.removeEventListener("resize", updateRadius);
+    }, []);
+
     useEffect(() => {
         const fetchData = async () => {
-          try {
-            const response = await api.get("/dashboard/"); 
-            console.log(response.data);
-            setTeams(response.data.teams);
-            setProjects(response.data.projects);
-            setHighPriorityTasks(response.data.highPriorityTasks);
-            setTaskStatus(response.data.taskStatus);
+            try {
+                const response = await api.get("/dashboard/");
+                console.log(response.data);
+                setTeams(response.data.teams);
+                setProjects(response.data.projects);
+                setHighPriorityTasks(response.data.highPriorityTasks);
+                setTaskStatus(response.data.taskStatus);
 
-            console.log(taskStatus);
-            
+                console.log(taskStatus);
 
-          } catch (error) {
-            console.error("Error fetching data:", error);
-          }
+
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
         };
         fetchData();
-      }, []);
-       
+    }, []);
 
 
-    return(
+
+    return (
         <section className="home-section">
-        <div className="w-layout-blockcontainer container w-container">
-            <div className="home-wrapper">
-                <div className="home-column">
-                    <h2 className="heading">My Teams</h2>
-                    <div className="home-column-list">
-                        {teams.map((team, index) => (
-                            <div key={index} className="home-column-list-item temp1">
-                                <div className="text-block-2">{team.teamName}</div>
-                                <div className="text-block-4">{team.teamDescription}</div>
-                                <div className="home-column-list-item-list">
-                                    {team.projects.map((project, index) => (
-                                        <div key={index} className="text-block-3 projects-list">{project}</div>
-                                    ))}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
-                <div className="home-column">
-                    <h2 className="heading">My Projects</h2>
-                    <div className="home-column-list">
-                        {projects.map((project, index) => (
-                            <div key={index} className="home-column-list-item temp2">
-                                <div className="text-block-2">{project.projectName}</div>
-                                <div className="text-block-6">{project.projectDescription}</div>
-                                <div className="home-column-list-item-list">
-                                    {project.tasks.map((task, index) => (
-                                        <div key={index} className="text-block-3 temp-dashboard">{task}</div>
-                                    ))}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div className="home-column column-2rows">
-                    <div className="home-important-tasks">
-                        <h2 className="heading">High Priority Tasks</h2>
+            <div className="w-layout-blockcontainer container w-container">
+                <div className="home-wrapper">
+                    <div className="home-column">
+                        <h2 className="heading">My Teams</h2>
                         <div className="home-column-list">
-                            {highPriorityTasks.map((task, index) => (
-                                <div key={index} className="home-column-list-item">
-                                    <div className="text-block-2">{task.taskName}</div>
-                                    <div className="text-block-6">{task.taskDescription}</div>
+                            {teams.map((team, index) => (
+                                <div key={index} className="home-column-list-item temp1">
+                                    <div className="text-block-2">{team.teamName}</div>
+                                    <div className="text-block-4">{team.teamDescription}</div>
                                     <div className="home-column-list-item-list">
-                                        <div className="text-block-3 ip-list">{task.status}</div>
+                                        {team.projects.map((project, index) => (
+                                            <div key={index} className="text-block-3 projects-list">{project}</div>
+                                        ))}
                                     </div>
                                 </div>
                             ))}
                         </div>
                     </div>
-                    <div className="home-tasks-chart">
-                        <div className="chart-map">
-                            <div className="text-block-7"></div>
-                            {taskStatus.map((status, index) => {
-                                let className = "text-block-8"; // Default class
-                                if (status.status === "InProgress") {
-                                    className = "text-block-9";
-                                } else if (status.status === "Done") {
-                                    className = "text-block-10";
-                                }
-                                return (
-                                    <div key={index} className={className}>
-                                        {status.status}: <span className="text-span">{status.count}</span>
-                                    </div>
-                                );
-                            })}
-                            <div className="text-block-12">Total: <span className="text-span">{taskStatus.reduce((acc, status) => acc + status.count, 0)}</span></div>
-                        </div>
-                        <div className="chart-div">
-                            <ResponsiveContainer width="100%" height="100%">
-                                <PieChart>
-                                    <Pie
-                                        data={taskStatus}
-                                        dataKey="count"
-                                        nameKey="status"
-                                        cx="50%"
-                                        cy="50%"
-                                        outerRadius={80}
-                                        fill="#8884d8"
-                                        label
-                                    >
-                                        {taskStatus.map((entry, index) => (
-                                            <Cell key={`cell-${index}`} fill={["#f2c94c", "#f2994a", "#27ae60", "#FF8042"][index % 4]} />
+
+                    <div className="home-column">
+                        <h2 className="heading">My Projects</h2>
+                        <div className="home-column-list">
+                            {projects.map((project, index) => (
+                                <div key={index} className="home-column-list-item temp2">
+                                    <div className="text-block-2">{project.projectName}</div>
+                                    <div className="text-block-6">{project.projectDescription}</div>
+                                    <div className="home-column-list-item-list">
+                                        {project.tasks.map((task, index) => (
+                                            <div key={index} className="text-block-3 temp-dashboard">{task}</div>
                                         ))}
-                                    </Pie>
-                                    <Tooltip />
-                                    <Legend />
-                                </PieChart>
-                            </ResponsiveContainer>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="home-column column-2rows">
+                        <div className="home-important-tasks">
+                            <h2 className="heading">High Priority Tasks</h2>
+                            <div className="home-column-list">
+                                {highPriorityTasks.map((task, index) => (
+                                    <div key={index} className="home-column-list-item">
+                                        <div className="text-block-2">{task.taskName}</div>
+                                        <div className="text-block-6">{task.taskDescription}</div>
+                                        <div className="home-column-list-item-list">
+                                            <div className="text-block-3 ip-list">{task.status}</div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="home-tasks-chart">
+                            <div className="chart-map">
+                                <div className="text-block-7"></div>
+                                {taskStatus.map((status, index) => {
+                                    let className = "text-block-8"; // Default class
+                                    if (status.status === "InProgress") {
+                                        className = "text-block-9";
+                                    } else if (status.status === "Done") {
+                                        className = "text-block-10";
+                                    }
+                                    return (
+                                        <div key={index} className={className}>
+                                            {status.status}: <span className="text-span">{status.count}</span>
+                                        </div>
+                                    );
+                                })}
+                                <div className="text-block-12">Total: <span className="text-span">{taskStatus.reduce((acc, status) => acc + status.count, 0)}</span></div>
+                            </div>
+                            <div className="chart-div">
+                                <div className="chart-wrapper" style={{ width: "100%", height: "100%" }}>
+                                    <ResponsiveContainer width="100%" height="100%">
+                                        <PieChart>
+                                            <Pie
+                                                data={taskStatus}
+                                                dataKey="count"
+                                                nameKey="status"
+                                                cx="50%"
+                                                cy="50%"
+                                                outerRadius={radius}
+                                                fill="#8884d8"
+                                                label
+                                            >
+                                                {taskStatus.map((entry, index) => (
+                                                    <Cell key={`cell-${index}`} fill={["#f2c94c", "#f2994a", "#27ae60", "#FF8042"][index % 4]} />
+                                                ))}
+                                            </Pie>
+                                            <Tooltip />
+                                            <Legend />
+                                        </PieChart>
+                                    </ResponsiveContainer>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
     );
 }
